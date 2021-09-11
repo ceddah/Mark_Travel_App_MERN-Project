cc7f4395d84553a883dc9e628272193922168a60
ceddah <cedanik1@gmail.com>
2021-09-11 14:57:53 +0200

    now users can mark the location on map for further interaction

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,12 @@ function App() {
     longitude: -95.665,
     zoom: 3
   });
+  const [addEntryLocation, setAddEntryLocation] = useState(null);
+
+  const showAddMarkerPopup = (e) => {
+    const [ longitude, latitude ] = e.lngLat;
+    setAddEntryLocation({ latitude, longitude });
+  };
 
   useEffect(() => {
     (async() => {
@@ -30,6 +36,7 @@ function App() {
       mapStyle="mapbox://styles/thecjreynolds/ck117fnjy0ff61cnsclwimyay"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={setViewport}
+      onDblClick={showAddMarkerPopup}
     >
       {logEntries.map(entry => (
         <React.Fragment>
@@ -74,6 +81,43 @@ function App() {
               ) : null}
         </React.Fragment>
           ))}
+        {addEntryLocation ? (
+          <>
+          <Marker
+            longitude={addEntryLocation.longitude} 
+            latitude={addEntryLocation.latitude} 
+            >
+              <svg
+                    className="marker red"
+                    style={{
+                      height: `${6 * viewport.zoom}px`,
+                      width: `${6 * viewport.zoom}px`,
+                    }}
+                    version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512">
+                    <g>
+                      <g>
+                        <path d="M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035
+                          c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719
+                          c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z"/>
+                      </g>
+                    </g>
+                  </svg>
+          </Marker>
+          <Popup
+              longitude={addEntryLocation.longitude} 
+              latitude={addEntryLocation.latitude} 
+              closeButton={true}
+              closeOnClick={false}
+              dynamicPosition={true}
+              onClose={() => setAddEntryLocation(null)}
+              anchor="top"
+              >
+                <div className="popup">
+                  <h3>Add you new Log Entry here:</h3>
+                </div>
+              </Popup> 
+          </>
+        ) : null }
     </ReactMapGL>
   );
 }
